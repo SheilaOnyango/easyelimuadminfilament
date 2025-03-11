c14abc5df390cabfc9f3038384839d39eb9d81af
Sheila Onyango <edusheila31@gmail.com>
2025-03-11 12:29:59 +0100

    Resource for Levels

---
+++ app/Filament/Resources/LevelResource.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\LevelResource\Pages;
+use App\Models\Level;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+
+class LevelResource extends Resource
+{
+    protected static ?string $model = Level::class;
+
+    protected static ?string $navigationGroup = 'Levels';
+
+    public static function query(): Builder
+    {
+        return Level::query()->latest();
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('id')->label('ID'),
+                Tables\Columns\TextColumn::make('name')->label('Name'),
+                Tables\Columns\TextColumn::make('type')->label('Type'),
+                Tables\Columns\TextColumn::make('description')->label('Description')->limit(50),
+                Tables\Columns\TextColumn::make('metadescription')->label('Meta Description')->limit(50),
+                Tables\Columns\TextColumn::make('alias')->label('Alias'),
+                Tables\Columns\TextColumn::make('created_at')->label('Created At')->dateTime(),
+                Tables\Columns\TextColumn::make('updated_at')->label('Updated At')->dateTime(),
+                Tables\Columns\TextColumn::make('image_name')->label('Image Name'),
+                Tables\Columns\TextColumn::make('created_by')->label('Created By'),
+                Tables\Columns\TextColumn::make('modified_by')->label('Modified By'),
+                Tables\Columns\TextColumn::make('status')->label('Status'),
+            ])
+            ->filters([])
+            ->actions([])
+            ->bulkActions([]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [];
+    }
+
+    public static function form(\Filament\Forms\Form $form): \Filament\Forms\Form
+    {
+        return $form
+            ->schema([
+                \Filament\Forms\Components\TextInput::make('name')->label('Name')->required(),
+                \Filament\Forms\Components\TextInput::make('type')->label('Type')->required(),
+                \Filament\Forms\Components\Textarea::make('description')->label('Description')->required(),
+                \Filament\Forms\Components\Textarea::make('metadescription')->label('Meta Description')->required(),
+                \Filament\Forms\Components\TextInput::make('alias')->label('Alias')->required(),
+                \Filament\Forms\Components\DateTimePicker::make('created_at')->label('Created At'),
+                \Filament\Forms\Components\DateTimePicker::make('updated_at')->label('Updated At'),
+                \Filament\Forms\Components\TextInput::make('image_name')->label('Image Name')->required(),
+                \Filament\Forms\Components\TextInput::make('created_by')->label('Created By')->required(),
+                \Filament\Forms\Components\TextInput::make('modified_by')->label('Modified By')->required(),
+                \Filament\Forms\Components\TextInput::make('status')->label('Status')->required(),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListLevels::route('/'),
+            'create' => Pages\CreateLevel::route('/create'),
+            'edit' => Pages\EditLevel::route('/{record}/edit'),
+        ];
+    }
+}
