4a65bbeb006f79bc193f02b8a6399f02eee5e3d2
Sheila Onyango <edusheila31@gmail.com>
2025-03-13 12:45:12 +0100

    Fix QuizResponse Model

--- app/Models/QuizResponse.php
+++ app/Models/QuizResponse.php
@@ -9,7 +9,7 @@ class QuizResponse extends Model
 {
     use HasFactory;
     
-    protected $table = 'quizresponses';
+    protected $table = 'quizreponses';
 
     protected $fillable = [
         'id', 'question_id', 'choice_id', 'is_user_correct_answer', 'user_id', 'lesson_id', 'created_at', 'updated_at'
