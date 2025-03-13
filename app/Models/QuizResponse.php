015cf55b5e1eed760e1627017e9e78ce71682bb3
Sheila Onyango <edusheila31@gmail.com>
2025-03-13 12:27:26 +0100

    QuizResponse Model

---
+++ app/Models/QuizResponse.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class QuizResponse extends Model
+{
+    use HasFactory;
+    
+    protected $table = 'quizresponses';
+
+    protected $fillable = [
+        'id', 'question_id', 'choice_id', 'is_user_correct_answer', 'user_id', 'lesson_id', 'created_at', 'updated_at'
+    ];
+}
+
