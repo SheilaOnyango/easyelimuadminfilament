1a3cc1e7f59f94d3674ddff366748e8a43c0de94
Sheila Onyango <edusheila31@gmail.com>
2025-03-14 14:37:22 +0100

    Resource for Sets

---
+++ app/Filament/Resources/SetResource.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SetResource\Pages;
+use App\Models\Set;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+
+class SetResource extends Resource
+{
+    protected static ?string $model = Set::class;
+
+    protected static ?string $navigationGroup = 'Sets';
+
+    public static function query(): Builder
+    {
+        return Set::query()->latest();
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('id')->label('ID'),
+                Tables\Columns\TextColumn::make('term_id')->label('Term ID'),
+                Tables\Columns\TextColumn::make('sets')->label('Sets'),
+                Tables\Columns\TextColumn::make('alias')->label('Alias'),
+                Tables\Columns\TextColumn::make('set_price')->label('Set Price'),
+                Tables\Columns\TextColumn::make('price_per_exam')->label('Price Per Exam'),
+                Tables\Columns\TextColumn::make('description')->label('Description')->limit(50),
+                Tables\Columns\TextColumn::make('metadescription')->label('Meta Description')->limit(50),
+                Tables\Columns\TextColumn::make('status')->label('Status'),
+                Tables\Columns\TextColumn::make('created_by')->label('Created By'),
+                Tables\Columns\TextColumn::make('modified_by')->label('Modified By'),
+                Tables\Columns\TextColumn::make('created_at')->label('Created At')->dateTime(),
+                Tables\Columns\TextColumn::make('updated_at')->label('Updated At')->dateTime(),
+            ])
+            ->filters([])
+            ->actions([])
+            ->bulkActions([]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [];
+    }
+
+    public static function form(\Filament\Forms\Form $form): \Filament\Forms\Form
+    {
+        return $form
+            ->schema([
+                \Filament\Forms\Components\TextInput::make('term_id')->label('Term ID')->required(),
+                \Filament\Forms\Components\TextInput::make('sets')->label('Sets')->required(),
+                \Filament\Forms\Components\TextInput::make('alias')->label('Alias')->required(),
+                \Filament\Forms\Components\TextInput::make('set_price')->label('Set Price')->required(),
+                \Filament\Forms\Components\TextInput::make('price_per_exam')->label('Price Per Exam')->required(),
+                \Filament\Forms\Components\Textarea::make('description')->label('Description')->required(),
+                \Filament\Forms\Components\Textarea::make('metadescription')->label('Meta Description')->required(),
+                \Filament\Forms\Components\TextInput::make('status')->label('Status')->required(),
+                \Filament\Forms\Components\TextInput::make('created_by')->label('Created By')->required(),
+                \Filament\Forms\Components\TextInput::make('modified_by')->label('Modified By')->required(),
+                \Filament\Forms\Components\DateTimePicker::make('created_at')->label('Created At'),
+                \Filament\Forms\Components\DateTimePicker::make('updated_at')->label('Updated At'),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSets::route('/'),
+            'create' => Pages\CreateSet::route('/create'),
+            'edit' => Pages\EditSet::route('/{record}/edit'),
+        ];
+    }
+}
