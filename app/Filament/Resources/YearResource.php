f53ee5d7f3ff6b022cd36ebb9c4629e97917172b
Sheila Onyango <edusheila31@gmail.com>
2025-03-26 14:28:22 +0200

    Resource for Years

---
+++ app/Filament/Resources/YearResource.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\YearResource\Pages;
+use App\Models\Year;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+
+class YearResource extends Resource
+{
+    protected static ?string $model = Year::class;
+
+    protected static ?string $navigationGroup = 'Academic';
+
+    public static function query(): Builder
+    {
+        return Year::query()->latest();
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('id')->label('ID'),
+                Tables\Columns\TextColumn::make('year')->label('Year'),
+                Tables\Columns\TextColumn::make('alias')->label('Alias'),
+                Tables\Columns\TextColumn::make('grade_id')->label('Grade ID'),
+                Tables\Columns\TextColumn::make('description')->label('Description'),
+                Tables\Columns\TextColumn::make('metadescription')->label('Meta Description'),
+                Tables\Columns\TextColumn::make('status')->label('Status'),
+                Tables\Columns\TextColumn::make('created_by')->label('Created By'),
+                Tables\Columns\TextColumn::make('modified_by')->label('Modified By'),
+                Tables\Columns\TextColumn::make('created_at')->label('Created At')->dateTime(),
+                Tables\Columns\TextColumn::make('updated_at')->label('Updated At')->dateTime(),
+            ])
+            ->filters([])
+            ->actions([])
+            ->bulkActions([]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [];
+    }
+
+    public static function form(\Filament\Forms\Form $form): \Filament\Forms\Form
+    {
+        return $form
+            ->schema([
+                \Filament\Forms\Components\TextInput::make('year')->label('Year')->required(),
+                \Filament\Forms\Components\TextInput::make('alias')->label('Alias')->required(),
+                \Filament\Forms\Components\TextInput::make('grade_id')->label('Grade ID')->numeric()->required(),
+                \Filament\Forms\Components\Textarea::make('description')->label('Description')->required(),
+                \Filament\Forms\Components\Textarea::make('metadescription')->label('Meta Description')->required(),
+                \Filament\Forms\Components\TextInput::make('status')->label('Status')->required(),
+                \Filament\Forms\Components\TextInput::make('created_by')->label('Created By')->required(),
+                \Filament\Forms\Components\TextInput::make('modified_by')->label('Modified By')->required(),
+                \Filament\Forms\Components\DateTimePicker::make('created_at')->label('Created At'),
+                \Filament\Forms\Components\DateTimePicker::make('updated_at')->label('Updated At'),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListYears::route('/'),
+            'create' => Pages\CreateYear::route('/create'),
+            'edit' => Pages\EditYear::route('/{record}/edit'),
+        ];
+    }
+}
